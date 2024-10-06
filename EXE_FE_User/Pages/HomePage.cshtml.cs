1067ddf3ef26feffd5f030dd7e4df26cd4bd6e01
Tran Viet Hoang <hoangdl939@gmail.com>
2024-10-06 21:44:52 +0700

    init project

---
+++ EXE_FE_User/Pages/HomePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EXE_FE_User.Pages
+{
+    public class HomePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
